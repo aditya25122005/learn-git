c9eb0ada7debdd8ec46def1c8399ce731f49a83a
aditya25122005 <aditya25pda@gmail.com>
2024-09-15 00:12:46 +0530

    Initial commit

---
+++ first.java
@@ -0,0 +1,8 @@
+import java.net.StandardSocketOptions;
+import java.sql.SQLOutput;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Aditya");
+    }
+}
